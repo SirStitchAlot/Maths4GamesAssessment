070b24d843d47725cf7df012eab8ef7986625f40
SamGerardGray <samgerardgray@gmail.com>
2024-06-05 16:43:33 +1000

    dasdasdsadsa

--- RaylibStarterCpp/RaylibStarterCpp/RaylibStarterCPP/main.cpp
+++ RaylibStarterCpp/RaylibStarterCpp/RaylibStarterCPP/main.cpp
@@ -54,7 +54,7 @@ int main(int argc, char* argv[])
 
         ClearBackground(BLACK);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Good Luck Sam", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
