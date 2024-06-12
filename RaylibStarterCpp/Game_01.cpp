928086393de65a3d46122ef4791eb9987e4f37c9
SamGerardGray <samgerardgray@gmail.com>
2024-06-13 00:16:25 +1000

    Update Game_01.cpp

--- RaylibStarterCpp/Game_01.cpp
+++ RaylibStarterCpp/Game_01.cpp
@@ -29,11 +29,11 @@ void Game_01::Shutdown() {
 void Game_01::Update() {
 	// detect keyboard input
 	if (IsKeyDown(KEY_A)) // code;
-		if (IsKeyDown(KEY_D)) // code;
-			if (IsKeyDown(KEY_W)) // code;
-				if (IsKeyDown(KEY_S)) // code;
+    if (IsKeyDown(KEY_D)) // code;
+	if (IsKeyDown(KEY_W)) // code;
+	if (IsKeyDown(KEY_S)) // code;
 
-					tankObject->Update(7); // just put 7 here because i want to stop getting errors
+	tankObject->Update(7); // just put 7 here because i want to stop getting errors
 
 
 }
